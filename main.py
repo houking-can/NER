32adfbe9bb6fc0822ab954db2dcaa8bdf43c20b2
houking-can <1240723224@qq.com>
2019-09-22 10:50:22 +0800

    add code

---
+++ main.py
@@ -0,0 +1,152 @@
+import tensorflow as tf
+import numpy as np
+import os, argparse, time, random
+from model import BiLSTM_CRF
+from utils import str2bool, get_logger, get_entity
+from data import read_corpus, read_dictionary, tag2label, random_embedding
+
+## Session configuration
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # default: 0
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+config.gpu_options.per_process_gpu_memory_fraction = 0.2  # need ~700MB GPU memory
+
+## hyperparameters
+parser = argparse.ArgumentParser(description='BiLSTM-CRF for Chinese NER task')
+parser.add_argument('--data', type=str, default='data', help='train, test, data source')
+parser.add_argument('--output', type=str, default='output', help='checkpoint,summaries,eval results')
+parser.add_argument('--batch_size', type=int, default=64, help='#sample of each minibatch')
+parser.add_argument('--epoch', type=int, default=10, help='#epoch of training')
+parser.add_argument('--hidden_dim', type=int, default=300, help='#dim of hidden state')
+parser.add_argument('--optimizer', type=str, default='Adam', help='Adam/Adadelta/Adagrad/RMSProp/Momentum/SGD')
+parser.add_argument('--CRF', type=str2bool, default=True, help='use CRF at the top layer. if False, use Softmax')
+parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+parser.add_argument('--clip', type=float, default=5.0, help='gradient clipping')
+parser.add_argument('--dropout', type=float, default=0.5, help='dropout keep_prob')
+parser.add_argument('--update_embedding', type=str2bool, default=True, help='update embedding during training')
+parser.add_argument('--pretrain_embedding', type=str, default='random',
+                    help='use pretrained char embedding or init it randomly')
+parser.add_argument('--embedding_dim', type=int, default=300, help='random init char embedding_dim')
+parser.add_argument('--shuffle', type=str2bool, default=True, help='shuffle training data before each epoch')
+parser.add_argument('--mode', type=str, default='demo', help='train/test/demo/predict')
+parser.add_argument('--predict_path', type=str, help='the data path for predict')
+parser.add_argument('--demo_model', type=str, default='1569033927', help='model for test and demo')
+
+args = parser.parse_args()
+
+## get char embeddings
+word2id = read_dictionary(os.path.join('.', args.data, 'word2id.pkl'))
+if args.pretrain_embedding == 'random':
+    embeddings = random_embedding(word2id, args.embedding_dim)
+else:
+    embedding_path = os.path.join('.', args.data, 'embedding.npy')
+    embeddings = np.array(np.load(embedding_path), dtype='float32')
+
+## read corpus and get training data
+if args.mode != 'demo':
+    train_data = read_corpus(os.path.join('.', args.data, 'train'))
+    test_data = read_corpus(os.path.join('.', args.data, 'test'))
+
+## paths setting
+paths = {}
+timestamp = str(int(time.time())) if args.mode == 'train' else args.demo_model
+output_path = os.path.join('.', args.output, timestamp)
+if not os.path.exists(output_path): os.makedirs(output_path)
+summary_path = os.path.join(output_path, "summaries")
+paths['summary_path'] = summary_path
+if not os.path.exists(summary_path): os.makedirs(summary_path)
+model_path = os.path.join(output_path, "checkpoints/")
+if not os.path.exists(model_path): os.makedirs(model_path)
+ckpt_prefix = os.path.join(model_path, "model")
+paths['model_path'] = ckpt_prefix
+result_path = os.path.join(output_path, "results")
+paths['result_path'] = result_path
+if not os.path.exists(result_path): os.makedirs(result_path)
+log_path = os.path.join(result_path, "log.txt")
+paths['log_path'] = log_path
+get_logger(log_path).info(str(args))
+
+## training model
+if args.mode == 'train':
+    model = BiLSTM_CRF(args, embeddings, tag2label, word2id, paths, config=config)
+    model.build_graph()
+
+    ## hyperparameters-tuning, split train/dev
+    # dev_data = train_data[:5000]; dev_size = len(dev_data)
+    # train_data = train_data[5000:]; train_size = len(train_data)
+    # print("train data: {0}\ndev data: {1}".format(train_size, dev_size))
+    # model.train(train=train_data, dev=dev_data)
+
+    ## train model on the whole training data
+    print("train data: {}".format(len(train_data)))
+    model.train(train=train_data, dev=test_data)  # use test_data as the dev_data to see overfitting phenomena
+
+## testing model
+elif args.mode == 'test':
+    ckpt_file = tf.train.latest_checkpoint(model_path)
+    print(ckpt_file)
+    paths['model_path'] = ckpt_file
+    model = BiLSTM_CRF(args, embeddings, tag2label, word2id, paths, config=config)
+    model.build_graph()
+    print("test data: {}".format(len(test_data)))
+    model.test(test_data)
+
+## demo
+elif args.mode == 'demo':
+    ckpt_file = tf.train.latest_checkpoint(model_path)
+    print(ckpt_file)
+    paths['model_path'] = ckpt_file
+    model = BiLSTM_CRF(args, embeddings, tag2label, word2id, paths, config=config)
+    model.build_graph()
+    saver = tf.train.Saver()
+    with tf.Session(config=config) as sess:
+        print('============= demo =============')
+        saver.restore(sess, ckpt_file)
+        while (1):
+            print('Please input your sentence:')
+            demo_sent = input()
+            if demo_sent == '' or demo_sent.isspace():
+                print('See you next time!')
+                break
+            else:
+                demo_sent = list(demo_sent.strip())
+                demo_data = [(demo_sent, ['O'] * len(demo_sent))]
+                tag = model.demo_one(sess, demo_data)
+                # name 参照 data.py tag2label
+                PER = get_entity(tag, demo_sent, 'PER')
+                SEX = get_entity(tag, demo_sent, 'SEX')
+                TIT = get_entity(tag, demo_sent, 'TIT')
+                REA = get_entity(tag, demo_sent, 'PER')
+                print('PER: {}\nSEX: {}\nTIT: {}\nREA: {}'.format(PER, SEX, TIT, REA))
+
+                # E1 = get_entity(tag, demo_sent, '1通类')
+                # E2 = get_entity(tag, demo_sent, '2工程建设')
+                # E3 = get_entity(tag, demo_sent, '3工务')
+                # E4 = get_entity(tag, demo_sent, '4牵引动力')
+                # E5 = get_entity(tag, demo_sent, '5车辆')
+                # E6 = get_entity(tag, demo_sent, '6运输管理')
+                # E7 = get_entity(tag, demo_sent, '7通信')
+                # E8 = get_entity(tag, demo_sent, '8信号')
+                # print('1通类: {}\n2工程建设: {}\n3工务: {}\n4牵引动力: {}\n'
+                #       '5车辆: {}\n6运输管理: {}\n7通信: {}\n8信号: {}'.format(E1, E2, E3, E4, E5, E6, E7, E8))
+
+
+## predict
+elif args.mode == "predict":
+    ckpt_file = tf.train.latest_checkpoint(model_path)
+    print(ckpt_file)
+    paths['model_path'] = ckpt_file
+    model = BiLSTM_CRF(args, embeddings, tag2label, word2id, paths, config=config)
+    model.build_graph()
+    saver = tf.train.Saver()
+    with tf.Session(config=config) as sess:
+        print('============= predict =============')
+        saver.restore(sess, ckpt_file)
+        for file in os.listdir(args.predict_path):
+            print(file)
+            # sent = list(demo_sent.strip())
+            # data = [(demo_sent, ['O'] * len(demo_sent))]
+            # tag = model.demo_one(sess, demo_data)
+            # PER, SEX, TIT, REA = get_entity(tag, demo_sent)
+            # print('PER: {}\nSEX: {}\nTIT: {}\nREA: {}'.format(PER, SEX, TIT, REA))
